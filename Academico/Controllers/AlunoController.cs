2156e046c5854fc3b5ac67de1b9423b1265f9086
Breno Couto <breno.silva@ad.prefeitura.intranet>
2023-08-17 13:15:35 -0300

    terminando o projeto

---
+++ Academico/Controllers/AlunoController.cs
@@ -0,0 +1,28 @@
+﻿using Academico.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academico.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private static IList<Aluno> alunos = new List<Aluno>()
+        {
+            new Aluno()
+            {
+                AlunoID = 1,
+                Nome = "Luan",
+                Matricula = "212021202120"
+            },
+            new Aluno()
+            {
+                AlunoID = 2,
+                Nome = "Santana",
+                Matricula = "66666666666"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(alunos);
+        }
+    }
+}
